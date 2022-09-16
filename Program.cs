2b3f35cd64a69edee48f22423d3fc39c1071c06e
Eduardo_Reis <eduardoluisreis@gmail.com>
2022-09-16 15:56:35 -0300

    Exercicios em C#

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        string? texto1, texto2, texto3, texto4, auxiliar;
+
+        Console.WriteLine("Aplicativo Console para Inverter a ordem dos textos digitados.\n");
+
+        Console.Write("Digite o texto 1: ");
+        texto1 = Console.ReadLine();
+
+        Console.Write("Digite o texto 2: ");
+        texto2 = Console.ReadLine();
+
+        Console.Write("Digite o texto 3: ");
+        texto3 = Console.ReadLine();
+
+        Console.Write("Digite o texto 4: ");
+        texto4 = Console.ReadLine();
+
+        //Mecanismo pra inverter os textos
+        auxiliar = texto1;
+        texto1 = texto4;
+        texto4 = auxiliar;
+        auxiliar = texto2;
+        texto2 = texto3;
+        texto3 = auxiliar;
+
+        Console.WriteLine();
+        Console.WriteLine("Textos inseridos na ordem inversa\n");
+        Console.WriteLine(texto1);
+        Console.WriteLine(texto2);
+        Console.WriteLine(texto3);
+        Console.WriteLine(texto4);
+        Console.WriteLine("\nPressione enter para finalizar! ");
+        Console.ReadKey();
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Exercícios de fixação em C#
+
+## invert-text
+
+1. ***Aplicativo console para inverter nomes***
+
+   <p align="center">
+       <img width="1024" height="500" src="assets/invertenumber.gif">
+   </p>
+
+​																Exercicio elaborado através de aulas do Curso de C# da [Udemy](https://www.udemy.com/course/csharp-completo-e-profissional)
+
