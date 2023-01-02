ac8d54c74dc3e4cc0df49ad1e7a04d35cc00fb5e
Eduardo <eduardo@T-480.T-480.org>
2023-01-02 19:48:38 -0300

    incluindo exercicio codigo tabuada

---
+++ tabuada/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program{
+
+public static void Main(){
+
+    Console.WriteLine("Digite um número no qual deseja saber a tabuada!");
+      
+    int N = int.Parse(Console.ReadLine());
+    
+      tabuada(N, 0);
+            
+    }
+   public static void tabuada(int N, int fm){
+
+     if (fm > 10)
+       return;
+  
+   Console.WriteLine (N + "x" + fm + "=" + N * fm);
+   tabuada (N, fm + 1);
+   
+   }
+   
+ }
